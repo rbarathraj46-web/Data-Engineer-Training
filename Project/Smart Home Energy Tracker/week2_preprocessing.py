4069b5c8f08978d9272f4c4af587149bc9781827
BarathrajDataEngineer <rbarathraj46@gmail.com>
2025-09-25 17:12:47 +0530

    Update week2_preprocessing.py

--- Project/Smart Home Energy Tracker/week2_preprocessing.py
+++ Project/Smart Home Energy Tracker/week2_preprocessing.py
@@ -7,11 +7,9 @@ df['timestamp'] = pd.to_datetime(df['timestamp'], errors='coerce')
 df['energy_kwh'] = pd.to_numeric(df['energy_kwh'], errors='coerce')
 df.dropna(subset=['timestamp','device_id','room_id','energy_kwh'], inplace=True)
 
-# Device level
 device_summary = df.groupby('device_id')['energy_kwh'].agg(
     total_energy=np.sum, avg_energy=np.mean).reset_index()
 
-# Room level
 room_summary = df.groupby('room_id')['energy_kwh'].sum().reset_index()
 room_summary.rename(columns={'energy_kwh':'total_room_energy'}, inplace=True)
 
